cbc9d5ad9e7d554b2734c2e49519ea26d7343e19
kyyhkynen <kimmo.kyyhkynen@gmail.com>
2023-10-03 08:32:42 +0300

    support any a1111 api call

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -3,57 +3,75 @@ import time
 import runpod
 import requests
 from requests.adapters import HTTPAdapter, Retry
+from runpod.serverless.modules.rp_logger import RunPodLogger
 
+BASE_URL = "http://127.0.0.1:3000"
+TIMEOUT = 600
 
-automatic_session = requests.Session()
+session = requests.Session()
 retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
-automatic_session.mount('http://', HTTPAdapter(max_retries=retries))
+session.mount("http://", HTTPAdapter(max_retries=retries))
+logger = RunPodLogger()
 
 
 # ---------------------------------------------------------------------------- #
 #                              Automatic Functions                             #
 # ---------------------------------------------------------------------------- #
 def wait_for_service(url):
-    '''
+    """
     Check if the service is ready to receive requests.
-    '''
+    """
+    retries = 0
     while True:
         try:
             requests.get(url)
             return
         except requests.exceptions.RequestException:
-            print("Service not ready yet. Retrying...")
+            # Only log every 15 retries so the logs don't get spammed
+            if retries % 15 == 0:
+                logger.info("Service not ready yet. Retrying...")
+            retries += 1
         except Exception as err:
-            print("Error: ", err)
+            logger.error(f"Error: {err}")
 
         time.sleep(0.2)
 
 
-def run_inference(inference_request):
-    '''
-    Run inference on a request.
-    '''
-    response = automatic_session.post(url='http://127.0.0.1:3000/sdapi/v1/txt2img',
-                                      json=inference_request, timeout=600)
-    return response.json()
+def send_get_request(endpoint):
+    return session.get(url=f"{BASE_URL}/{endpoint}", timeout=TIMEOUT)
+
+
+def send_post_request(endpoint, payload):
+    return session.post(url=f"{BASE_URL}/{endpoint}", json=payload, timeout=TIMEOUT)
 
 
 # ---------------------------------------------------------------------------- #
 #                                RunPod Handler                                #
 # ---------------------------------------------------------------------------- #
 def handler(event):
-    '''
+    """
     This is the handler function that will be called by the serverless.
-    '''
+    """
 
-    json = run_inference(event["input"])
+    method = event["input"]["method"]
+    endpoint = event["input"]["endpoint"]
+    payload = {}
+    if "payload" in event["input"]:
+        payload = event["input"]["payload"]
 
-    # return the output that you want to be returned like pre-signed URLs to output artifacts
-    return json
+    try:
+        if method == "GET":
+            response = send_get_request(endpoint)
+        elif method == "POST":
+            response = send_post_request(endpoint, payload)
+    except Exception as e:
+        return {"error": str(e)}
+
+    return response.json()
 
 
 if __name__ == "__main__":
-    wait_for_service(url='http://127.0.0.1:3000/sdapi/v1/txt2img')
+    wait_for_service(url=f"{BASE_URL}/sdapi/v1/sd-models")
 
     print("WebUI API Service is ready. Starting RunPod...")
 
