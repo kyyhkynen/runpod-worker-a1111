6309cd069ebce5f3133150fa4561bc8cb372709d
kyyhkynen <kimmo.kyyhkynen@gmail.com>
2023-10-03 08:32:07 +0300

    preload clip-interrogator models

--- builder/cache.py
+++ builder/cache.py
@@ -1,6 +1,16 @@
+print("Preloading interrogate models")
+
 from webui import initialize
 import modules.interrogate
+
 initialize.initialize()
 interrogator = modules.interrogate.InterrogateModels("interrogate")
 interrogator.load()
 interrogator.categories()
+
+print("Preloading clip-interrogator models")
+
+from clip_interrogator import Config, Interrogator
+
+config = Config()
+interrogator = Interrogator(config)
